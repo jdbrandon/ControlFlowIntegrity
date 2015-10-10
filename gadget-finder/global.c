4daa40448ac8287ba3ffeb5eb4c7d7df75c55002
tiant900114 <tiant@andrew.cmu.edu>
2015-10-09 22:29:00 -0400

    command line argument parse

---
+++ gadget-finder/global.c
@@ -0,0 +1,5 @@
+/* all global variables */
+
+char infile[256];
+char outfile[256];
+
---
+++ gadget-finder/global.h
@@ -0,0 +1,12 @@
+#ifndef GLOBAL_HEADER
+#define GLOBAL_HEADER
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+void check_arguments(int, char**);
+
+#endif
