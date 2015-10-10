fae46bbce06625efd1446e026c1731b02399d1ed
tiant900114 <tiant@andrew.cmu.edu>
2015-10-09 23:15:09 -0400

    Update input.c
    Parse command line argument

--- gadget-finder/input.c
+++ gadget-finder/input.c
@@ -1,1 +1,60 @@
 /* input handling, commandline argument parsing */
+
+#include "global.h"
+
+extern char infile[], outfile[];
+
+void check_arguments(int argc, char *argv[])
+{
+	outfile[0] = 0x0;
+	infile[0] = 0x0;
+
+	for (int i=1; i<argc; i++) {
+		//parse commandline options
+		if (argv[i][0] == '-') {
+			//disallow combining options. don't bother to implement
+			if (argv[i][2]) {
+				fprintf(stderr, "Invalid option %s.\n", argv[i]);
+				exit(1);
+			}
+			switch (argv[i][1]) {
+			case 'h':
+				printf("Usage: %s [options] filename.\n", argv[0]);
+				exit(0);
+			case 'o':
+				if (outfile[0]) {
+					fprintf(stderr, "Duplicate output file.\n");
+					exit(1);
+				}
+				i = i + 1;
+				if (i == argc) {
+					fprintf(stderr, "Missing output file name.\n");
+					exit(1);
+				}
+				if (strlen(argv[i]) > 255) {
+					fprintf(stderr, "Output file name is too long.\n");
+					exit(1);
+				}
+				strcpy(outfile, argv[i]);
+			}
+		}
+		
+		//parse non-option arguments
+		else {
+			if (infile[0]) {
+				fprintf(stderr, "Duplicate input file name.\n");
+				exit(1);
+			}
+			if (strlen(argv[i]) > 255) {
+				fprintf(stderr, "Input file name is too long.\n");
+				exit(1);
+			}
+			strcpy(infile, argv[i]);
+		}
+	}
+	
+	if (!infile[0]) {
+		fprintf(stderr, "Missing input file name.\n");
+		exit(1);
+	}
+}
