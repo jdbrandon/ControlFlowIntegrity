254603bbc5cbb053861f01934300a9caddabcdbd
Jeff Brandon <jeffbrandon2010@gmail.com>
2015-10-13 11:57:17 -0700

    tabs -> spaces

--- gadget-finder/main.c
+++ gadget-finder/main.c
@@ -14,68 +14,66 @@ void write_output(size_t, char*);
 
 int main(int argc, char *argv[])
 {
+    size_t len, count = 1;
+    char last_instr_hex[INSTR_SIZE_MAX] = "", output_buffer[BUF_SIZE] = "";
+    FILE* input_fd;
+    const char* tmp;
+    struct section *s = parse_elf_file();
+    struct section *s1 = s;
     check_arguments(argc, argv);
+
     printf("input file: %s\n", infile);
     printf("output file: %s\n", outfile);
     
-	struct section *s = parse_elf_file();
-	struct section *s1 = s;
-	
-	while (s1 != NULL) {
-		printf("%s\n", s1->sh_name);
-		size_t len, count = 1;
+    while (s1 != NULL) {
+        printf("%s\n", s1->sh_name);
+        if((input_fd = fopen(s1->sh_name, "rb")) == NULL) {
+            fprintf(stderr,"Error opening input file: %s\n", s1->sh_name);
+            exit(1);
+        }
 
-		char last_instr_hex[INSTR_SIZE_MAX] = "", output_buffer[BUF_SIZE] = "";
-		
-		FILE* input_fd;
-		if((input_fd = fopen(s1->sh_name, "rb")) == NULL) {
-			fprintf(stderr,"Error opening input file: %s\n", s1->sh_name);
-			exit(1);
-		}
-		
-		ud_t disassembly_obj;
-		ud_init(&disassembly_obj);
-		ud_set_input_file(&disassembly_obj, input_fd);
-		ud_set_mode(&disassembly_obj, 64);
-		ud_set_syntax(&disassembly_obj, UD_SYN_INTEL);
-		ud_set_pc(&disassembly_obj, s1->vaddr);	
-	
-		//TODO: Add special rules for NSA instrumented instructions
-		while(ud_disassemble(&disassembly_obj)) {
-			const char *tmp = ud_insn_hex(&disassembly_obj);
-			
-			//This coalesces repeated instructions to make output more compressed / readable
-			if(strcmp(last_instr_hex, tmp) == 0) {
-		    	count++;
-		    	continue;
-		    }			
+        ud_t disassembly_obj;
+        ud_init(&disassembly_obj);
+        ud_set_input_file(&disassembly_obj, input_fd);
+        ud_set_mode(&disassembly_obj, 64);
+        ud_set_syntax(&disassembly_obj, UD_SYN_INTEL);
+        ud_set_pc(&disassembly_obj, s1->vaddr);	
 
-			write_output(count, output_buffer);
-			count = 1;
+        //TODO: Add special rules for NSA instrumented instructions
+        while(ud_disassemble(&disassembly_obj)) {
+            tmp = ud_insn_hex(&disassembly_obj);
 
-			len = snprintf(output_buffer, BUF_SIZE, "0x%.8lx\t%s%s%s", (long unsigned int) ud_insn_off(&disassembly_obj), ud_insn_asm(&disassembly_obj),"%s","%s");
-			snprintf_errcheck(len, BUF_SIZE);
+            //This coalesces repeated instructions to make output more compressed / readable
+            if(strcmp(last_instr_hex, tmp) == 0) {
+                count++;
+                continue;
+            }
 
-		    //update last_instr for repeated instruction coalescing
-			len = strlen(tmp);
-			strncpy(last_instr_hex, tmp, len);
-			last_instr_hex[len] = 0;
-		}
-		
-		//flush last output	
+            write_output(count, output_buffer);
+            count = 1;
+
+            len = snprintf(output_buffer, BUF_SIZE, "0x%.8lx\t%s%s%s", (long unsigned int) ud_insn_off(&disassembly_obj), ud_insn_asm(&disassembly_obj),"%s","%s");
+            snprintf_errcheck(len, BUF_SIZE);
+
+            //update last_instr for repeated instruction coalescing
+            len = strlen(tmp);
+            strncpy(last_instr_hex, tmp, len);
+            last_instr_hex[len] = 0;
+        }
+        //flush last output	
 		write_output(count, output_buffer);
-		
-		//Parse Disassembly
 
-		//Display Gadgets
-		
-		fclose(input_fd);
-		s1 = s1->next;
-		
-		printf("*********************************************************\n");
-	}
+        //Parse Disassembly
 
-	free(s);
+        //Display Gadgets
+
+        fclose(input_fd);
+        s1 = s1->next;
+
+        printf("*********************************************************\n");
+    }
+
+    free(s);
     return 0;
 }
 
